ca75e59e106467aca814fbc2a1e6af6615acc831
Vatsan <118588346+vatsan-murali@users.noreply.github.com>
2023-04-17 22:02:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ const bodyParser = require('body-parser');
 app.use(cookieParser())
 app.use(express.json())
 app.use(bodyParser.json());
-app.use(cors({
-    origin:["https://mfrontend-nu.vercel.app","http://44.211.176.112"]
-    }))
+app.use(cors())
 
 dotenv.config({path:'./config.env'})
 require('./db/conn')
