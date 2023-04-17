2948b5586f6db4fb1c068954df992652c5077cc6
Vatsan <118588346+vatsan-murali@users.noreply.github.com>
2023-04-17 19:08:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(bodyParser.json());
 app.use(cors({
-    origin:["https://mfrontend-nu.vercel.app/","http://44.211.176.112"]
+    origin:["https://mbackend-lilac.vercel.app","http://44.211.176.112"]
     }))
 
 dotenv.config({path:'./config.env'})
