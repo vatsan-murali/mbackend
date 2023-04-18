c8fb58dbc03b1dc14efa1f7b32da55f0b4c7debf
Vatsan <118588346+vatsan-murali@users.noreply.github.com>
2023-04-18 20:52:49 +0530

    Update authenticate.js

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -5,9 +5,10 @@ const Authenticate = async (req,res, next) => {
     try {
         //const token = req.cookies.jwtoken;
         const token = req.headers.authorization.split(" ")[1];
-        console.log(token)
+        console.log('authenticate:',token)
         const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
         const { userId, username } = verifyToken;
+        console.log('userid, username:',userId, username)
         console.log('id'+verifyToken._id)
         const rootUser = await User.findOne({_id: userId, "tokens.token": token}); 
         console.log(rootUser)
@@ -19,6 +20,7 @@ const Authenticate = async (req,res, next) => {
         req.rootUser = rootUser;
         req.userID = rootUser._id;
         req.username = username;
+        console.log('authenticated')
         next();
     } catch(e) {
         res.status(401).send('No token')
